83c49a4b4a1f950a4b2d2d23f8eff1172bae2034
boazturya <boazturya@gmail.com>
2021-09-17 11:32:26 -0500

    added reactAndSpringApplication.java

---
+++ crm/src/main/java/com/crm/crm/model/ReactAndSpringDataApplicaion.java
@@ -0,0 +1,12 @@
+package com.crm.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataApplicaion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataApplicaion.class,args);
+    }
+}
